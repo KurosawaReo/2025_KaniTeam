4e3267e7837116aa454d00183139768ee3711285
star <KurosawaReo.Job@gmail.com>
2025-10-03 02:44:02 +0900

    ブロックの接着仮完成

---
+++ 2025_KaniTeam/Assets/Scripts/Block/BlockFly.cs
@@ -0,0 +1,40 @@
+/*
+   ブロックをくっつけるプログラム.
+   担当: 黒澤
+*/
+using UnityEngine;
+
+/// <summary>
+/// 仮, こっちはブロックを渡す側.
+/// </summary>
+public class BlockFly : BlockBase
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// 衝突した時に作動.
+    /// </summary>
+    private void OnCollisionEnter2D(Collision2D c)
+    {
+        //設置されてるブロックなら.
+        if (c.gameObject.CompareTag("block_no_fly"))
+        {
+            //予め数を取得する(ループ中に数が変わるため)
+            int cnt = transform.childCount;
+            //全ての子オブジェクト.
+            for (int i = 0; i < cnt; i++)
+            {
+                var obj = transform.GetChild(0); //先頭のobjを取得.
+                obj.SetParent(c.transform);      //衝突したobjに移動する.
+            }
+        }
+    }
+}
