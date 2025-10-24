d3b334442ba9b8f63387195d976bc046e3e7ef9e
star <KurosawaReo.Job@gmail.com>
2025-10-24 17:41:27 +0900

    ドダイジンベエとキュウバンコバン仮完成、大まかに挙動は問題なし？

--- 2025_KaniTeam/Assets/Scripts/Fish/FishGenerator.cs
+++ 2025_KaniTeam/Assets/Scripts/Fish/FishGenerator.cs
@@ -1,19 +1,21 @@
 using UnityEngine;
 using KR_Lib.Timer;
 using KR_Lib.Object;
+using System.Collections.Generic;
 
 /// <summary>
-/// 仮, ブロックを生成する.
+/// テスト用, 魚を生成する.
 /// </summary>
 public class FishGenerator : MonoBehaviour
 {
     [Header("- prefab -")]
-    [SerializeField] PrefabKR fish;
+    [SerializeField]         List<PrefabKR> fish; //魚's.
+    [SerializeField, Min(0)] int fishNo;          //何番目の魚を使うか.
 
     [Header("- value -")]
     [SerializeField] Vector3 spawnPos;
 
-    TimerKR timer = new TimerKR(1.0f); //タイマー作成.
+//  TimerKR timer = new TimerKR(1.0f); //タイマー作成.
 
     void Start()
     {
@@ -23,11 +25,14 @@ public class FishGenerator : MonoBehaviour
     void Update()
     {
         //タイマー減少.
-        timer.TimerDown();
+//      timer.TimerDown();
         //一定時間ごと.
-        if (timer.IntervalTime())
+//      if (timer.IntervalTime())
+
+        //デバッグ用.
+        if (Input.GetKeyDown(KeyCode.M))
         {
-            var obj = fish.NewPrefab();
+            var obj = fish[fishNo].NewPrefab();
             obj.transform.position = spawnPos; //位置設定.
         }
     }
